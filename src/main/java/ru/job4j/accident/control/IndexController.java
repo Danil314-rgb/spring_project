cdb77a1b7163edd1539a75811a5769b59ffd44c9
Danil <totem7h@gmail.com>
2022-04-11 15:14:05 +0400

    Initial commit

---
+++ src/main/java/ru/job4j/accident/control/IndexController.java
@@ -0,0 +1,18 @@
+package ru.job4j.accident.control;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/accident")
+    public String index(Model model) {
+        List<String> users = List.of("Tom", "Bob", "Lol");
+        model.addAttribute("users", users);
+        return "index";
+    }
+}
